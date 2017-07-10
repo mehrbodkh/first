abd2c0c5b17243079883900edeaa5fd4d844f10e
Mehrbod Khiabani <mehrbod.kh@gmail.com>
2017-07-10 15:05:21 +0430

    Completely changed.

--- Hello.java
+++ Hello.java
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 
 
 public class Hello {
-	
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		add(1+2);
 	}
 }
-=======
-public class Hello
-{
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-
-	public static int add(int x, int y) {
-		int a = x + y;
-		return a;
-	}
-}
->>>>>>> 0a854a9b7e1ca27308a320ff2b0dee6ca0b657ac
